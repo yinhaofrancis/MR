1c1d0de4085f30bfb9fd24e47357b61a854309bd
yinhao <yinhao@5eplay.com>
2023-12-21 21:39:12 +0800

    mm

---
+++ MRRender/MRRender/MRModel.cpp
@@ -0,0 +1,8 @@
+//
+//  MRModel.cpp
+//  MRRender
+//
+//  Created by wenyang on 2023/12/18.
+//
+
+#include "MRModel.hpp"
---
+++ MRRender/MRRender/MRModel.hpp
@@ -0,0 +1,13 @@
+//
+//  MRModel.hpp
+//  MRRender
+//
+//  Created by wenyang on 2023/12/18.
+//
+
+#ifndef MRModel_hpp
+#define MRModel_hpp
+
+#include <stdio.h>
+
+#endif /* MRModel_hpp */
